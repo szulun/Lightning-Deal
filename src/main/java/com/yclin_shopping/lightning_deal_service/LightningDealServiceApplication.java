216398158a4e2f63e3de629e07abdd38eba5c4b4
Yi-Chang Lin <kiraskywing@gmail.com>
2022-06-22 20:59:25 +0800

    init project

---
+++ src/main/java/com/yclin_shopping/lightning_deal_service/LightningDealServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yclin_shopping.lightning_deal_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightningDealServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightningDealServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yclin_shopping/lightning_deal_service/LightningDealServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yclin_shopping.lightning_deal_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightningDealServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
