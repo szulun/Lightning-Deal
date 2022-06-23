c42fd50e7fb4b44a96221a9a104c1994b2e29196
Yi-Chang Lin <kiraskywing@gmail.com>
2022-06-23 20:46:48 +0800

    Static page generation module

--- src/main/java/com/yclin_shopping/lightning_deal_service/controller/SeckillActivityController.java
+++ src/main/java/com/yclin_shopping/lightning_deal_service/controller/SeckillActivityController.java
@@ -3,6 +3,7 @@ package com.yclin_shopping.lightning_deal_service.controller;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSON;
@@ -182,5 +184,13 @@ public class SeckillActivityController {
     public String payOrder(@PathVariable String orderNo) throws Exception {
         seckillActivityService.payOrderProcess(orderNo);
         return "redirect:/seckill/orderQuery/" + orderNo;
+    }
+
+    @ResponseBody
+    @RequestMapping("/seckill/getSystemTime")
+    public String getSystemTime() {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date = df.format(new Date());
+        return date;
     }
 }
---
+++ src/test/java/com/yclin_shopping/lightning_deal_service/ThymeleafServiceTest.java
@@ -0,0 +1,20 @@
+package com.yclin_shopping.lightning_deal_service;
+
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.yclin_shopping.lightning_deal_service.service.ActivityHtmlPageService;
+
+@SpringBootTest
+public class ThymeleafServiceTest {
+
+    @Resource
+    private ActivityHtmlPageService activityHtmlPageService;
+
+    @Test
+    public void createHtmlTest() {
+        activityHtmlPageService.createActivityHtml(19);
+    }
+}
